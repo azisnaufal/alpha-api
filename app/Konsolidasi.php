7c64a5bbc6c5c5c28a57717b09413413f01bc27d
aditgege <aditgege0@gmail.com>
2017-11-08 15:19:19 +0700

    where acan

--- app/Konsolidasi.php
+++ app/Konsolidasi.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace alpha-api;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/konsolidasi', 'KonsolidasiController@index');
 Route::get('/konsolidasi/jumlah/penduduk_perkelurahan', 'KonsolidasiController@jumlah_penduduk_perkelurahan');
+Route::get('/konsolidasi/jumlah/kelurahan_perkecamatan','KonsolidasiController@jumlah_kelurahan_perkecamatan');
+Route::get('/konsolidasi/data/penduduk_perkelurahan','KonsolidasiController@data_kependudukan_perkelurahan');
+Route::get('/konsolidasi/data/data_wilayah','KonsolidasiController@data_wilayah');
+Route::get('/konsolidasi/data/data_kelurahan_perkecamatan','KonsolidasiController@data_kelurahan_perkecamatan');
+Route::get('/konsolidasi/data/data_keagamaan_perkelurahan','KonsolidasiController@data_keagamaan_perkelurahan');
+Route::get('/konsolidasi/data/data_nama_kelurahan_perkecamatan','KonsolidasiController@nama_kelurahan_perkecamatan');
+Route::get('/konsolidasi/data/data_kelurahan','KonsolidasiController@data_kelurahan');
